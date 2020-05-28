1d5bbe98468de2a0db8a90430de9f79570decd9f
CharOConnell <charlotteoconnell@virginmedia.com>
2020-05-28 12:04:54 +0000

    Added new table and made the admin panel nice to see

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Item
+# from the current directory models folder, import the class
 # Register your models here.
+
+
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # characters/text in it to prevent unnamed items
+    done = models.BooleanField(null=False, blank=False, default=False)
+    # add the defaulted value
+
+    def __str__(self):
+        return self.name
+        # returns the name of the inputted thing
